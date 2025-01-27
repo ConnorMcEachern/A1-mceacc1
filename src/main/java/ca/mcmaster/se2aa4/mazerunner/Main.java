8faa01e285ec721876b97420d337fab2e72dc56c
Connor <mceachern.connor@gmail.com>
2025-01-27 11:52:29 -0500

    Adding apache CLI

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -5,6 +5,12 @@ import java.io.File;
 import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class Main {
 
@@ -12,9 +18,23 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("** Starting Maze Runner");
+
+        Options options = new Options();
+        options.addOption(Option.builder("i")
+            .hasArg(true)
+            .desc("MAZE_FILE")
+            .required(true)
+        .build());
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cdm = null;
+
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            CommandLine cmd = parser.parse( options, args);
+
+            String maze_file = cmd.getOptionValue("i");
+            System.out.println("**** Reading the maze from file " + maze_file);
+            BufferedReader reader = new BufferedReader(new FileReader(maze_file));
             String line;
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
