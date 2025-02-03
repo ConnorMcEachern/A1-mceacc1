21b21a39f8e97eec83a85fa47c1c643b79aef5ce
Connor <mceachern.connor@gmail.com>
2025-02-03 17:44:39 -0500

    Split main method

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -16,7 +16,33 @@ public class Main {
 
     public static void main(String[] args) {
         logger.debug("** Starting Maze Runner");
+        CommandLineParser parser = new DefaultParser();
 
+        try {
+            CommandLine cmd = parser.parse(getOptions(), args);
+
+            File maze_file = new File(cmd.getOptionValue("i"));
+            logger.debug("**** Reading the maze from file " + maze_file);
+            Maze maze = new Maze(maze_file);
+            if (cmd.hasOption("p")) {
+                logger.debug("**** Checking path");
+                checkPath(maze, cmd.getOptionValue("p"));
+            } else {
+                logger.debug("**** Computing path");
+                generatePath(maze);
+            }
+            logger.debug("** End of MazeRunner");
+
+        } catch(Exception e) {
+            System.err.println("/!\\ An error has occured /!\\");
+            System.err.println("PATH NOT COMPUTED");
+            logger.fatal(e.getLocalizedMessage());
+            System.exit(0);
+        }
+        
+    }
+
+    public static Options getOptions() {
         Options options = new Options();
         options.addOption(Option.builder("i")
             .hasArg(true)
@@ -29,40 +55,26 @@ public class Main {
             .desc("PATH_SEQUENCE")
             .required(false)
         .build());
-
-        CommandLineParser parser = new DefaultParser();
-        StringFactorizer factorizer = new StringFactorizer();
-
-        try {
-            CommandLine cmd = parser.parse(options, args);
-
-            File maze_file = new File(cmd.getOptionValue("i"));
-            logger.debug("**** Reading the maze from file " + maze_file);
-            Maze maze = new Maze(maze_file);
-            if (cmd.hasOption("p")) {
-                String pathString = cmd.getOptionValue("p");
-                logger.debug("**** Checking path");
-                Checker checker = new Checker();
-                boolean validPath = checker.checkPath(maze, factorizer.expand(pathString));
-                if (validPath) {
-                    System.out.println("correct path");
-                } else {
-                    System.out.println("inccorrect path");
-                }
-            } else {
-                logger.debug("**** Computing path");
-                MazeSolver solver = new RightHandRule();
-                String output = factorizer.factorize(solver.getPath(maze).toString());
-                System.out.println(output);
-            }
-            logger.debug("** End of MazeRunner");
-
-        } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
-            System.err.println("PATH NOT COMPUTED");
-            logger.fatal(e.getLocalizedMessage());
-            System.exit(0);
-        }
-        
+        return options;
     }
+
+    public static void checkPath(Maze maze, String pathString) {
+        StringFactorizer factorizer = new StringFactorizer();
+        Checker checker = new Checker();
+        boolean validPath = checker.checkPath(maze, factorizer.expand(pathString));
+        if (validPath) {
+            System.out.println("correct path");
+        } else {
+            System.out.println("inccorrect path");
+        }
+    }
+
+    public static void generatePath(Maze maze) {
+        StringFactorizer factorizer = new StringFactorizer();
+        MazeSolver solver = new RightHandRule();
+        String output = factorizer.factorize(solver.getPath(maze).toString());
+        System.out.println(output);
+    }
+
+
 }
