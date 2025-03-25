24ee6031122db81a6cb2678dab65af52c634ce63
Connor <mceachern.connor@gmail.com>
2025-03-25 19:24:23 -0400

    Added Unit tests for Direction and Position

---
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/DirectionTests.java
@@ -0,0 +1,34 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class DirectionTests {
+    @Test
+    public void testToString() {
+        assertEquals("RIGHT", Direction.Right().toString());
+        assertEquals("DOWN", Direction.Down().toString());
+        assertEquals("LEFT", Direction.Left().toString());
+        assertEquals("UP", Direction.Up().toString());
+    }
+
+    @Test
+    public void rights() {
+        Direction dir = Direction.Up();
+
+        assertTrue(Direction.Right().equals(dir.turnRight()));
+        assertTrue(Direction.Down().equals(dir.turnRight().turnRight()));
+        assertTrue(Direction.Left().equals(dir.turnRight().turnRight().turnRight()));
+        assertTrue(Direction.Up().equals(dir.turnRight().turnRight().turnRight().turnRight()));
+    }
+
+    @Test
+    public void lefts() {
+        Direction dir = Direction.Up();
+
+        assertTrue(Direction.Left() .equals(dir.turnLeft()));
+        assertTrue(Direction.Down() .equals(dir.turnLeft().turnLeft()));
+        assertTrue(Direction.Right().equals(dir.turnLeft().turnLeft().turnLeft()));
+        assertTrue(Direction.Up()   .equals(dir.turnLeft().turnLeft().turnLeft().turnLeft()));
+    }
+}
---
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/PositionTests.java
@@ -0,0 +1,34 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class PositionTests {
+    @Test
+    public void testToString() {
+        Position pos = new Position(4, 3);
+        
+        assertEquals("(4, 6)", pos.move(Direction.Right()).move(Direction.Right()).move(Direction.Right()).toString());
+        assertEquals("(4, 5)", pos.move(Direction.Right()).move(Direction.Right()).toString());
+        assertEquals("(4, 4)", pos.move(Direction.Right()).toString());
+        assertEquals("(4, 3)", pos.toString());
+
+    }
+
+    @Test
+    public void getters() {
+        Position pos = new Position(2, 3);
+        assertEquals(2, pos.getRow());
+        assertEquals(3, pos.getColumn());
+    }
+
+    @Test
+    public void moving() {
+        Position pos = new Position(0, 0);
+        assertTrue(pos.move(Direction.Up()).equals(new Position(-1, 0)));
+        assertTrue(pos.move(Direction.Right() ).equals(new Position(0, 1)));
+        assertTrue(pos.move(Direction.Down()).equals(new Position(1, 0)));
+        assertTrue(pos.move(Direction.Left() ).equals(new Position(0, -1)));
+        assertTrue(pos.equals(new Position(0, 0)));
+    }
+}
